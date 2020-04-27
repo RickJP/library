6cecae8e5177fb202a40e89549eacd858362bed1
Rick <{ID}+{username}@users.noreply.github.com>
2020-04-27 16:17:13 +0900

    Books Create Index/New

--- server.js
+++ server.js
@@ -6,23 +6,22 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
-const bodyParser  = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorsRouter = require('./routes/authors');
+const booksRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-//app.use(bodyParser.urlencoded({extended: true}));
-//app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ limit: '10mb', extended: false}));
 
 app.use('/', indexRouter);
 app.use('/authors', authorsRouter);
+app.use('/books', booksRouter);
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
